17ad9ff8005adf760b65b942487f861703318ba9
Wouter Lem <wouter@igne.nl>
2025-01-21 14:31:42 +0100

    Remove log

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -2,10 +2,8 @@ import { usePageTitle } from "lib/page-title";
 import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
-	const { t, i18n } = useTranslation();
+	const { t } = useTranslation();
 	usePageTitle(t("pages.home.title"), []);
-
-	console.log(i18n.resolvedLanguage);
 
 	return <h1>{t("pages.home.title")}</h1>;
 };
