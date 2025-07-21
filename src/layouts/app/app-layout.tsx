ea5fe605c66b930aa573753af3ad27036b33e7aa
Jorn Luiten <jorn@igne.nl>
2025-07-21 14:11:31 +0200

    move app header and fix logout

--- src/layouts/app/app-layout.tsx
+++ src/layouts/app/app-layout.tsx
@@ -1,6 +1,7 @@
 import AppHeader from "layouts/app/app-header";
 import { Outlet } from "react-router";
 import style from "./app-layout.module.scss";
+import AppHeader from "./header/app-header";
 
 const LoggedInLayout = () => {
 	return (
