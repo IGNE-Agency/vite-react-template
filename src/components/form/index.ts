02319551353b726ed3f21c7448ed4b427a893bc6
Jorn Luiten <jorn@igne.nl>
2026-02-05 15:07:48 +0100

    remove issues component and clean up translation files

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from "./button/button";
 export { default as Form } from "./form";
 export { default as Input } from "./input/input";
-export { default as Issues } from "./issues/issues";
 export { default as Select } from "./select/select";
