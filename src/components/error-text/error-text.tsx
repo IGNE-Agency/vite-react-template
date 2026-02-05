0fa49016caf9943f8d61d6dee0bb5037c7192a65
Jorn Luiten <jorn@igne.nl>
2026-02-05 14:55:46 +0100

    remove zorm

--- src/components/error-text/error-text.tsx
+++ src/components/error-text/error-text.tsx
@@ -1,11 +1,43 @@
-import type { HTMLAttributes } from "react";
+import { Fragment, type HTMLAttributes } from "react";
 import style from "./error-text.module.scss";
 
-type Props = HTMLAttributes<"p"> & {
-	el?: "p" | "span";
-};
+export type ErrorProp = string | string[];
 
-export function ErrorText({ el = "p", children }: Props) {
+type Props = HTMLAttributes<"p"> & {
+	children?: ErrorProp;
+} & (
+		| {
+				el?: "p" | "span";
+				htmlFor?: never;
+		  }
+		| {
+				el: "label";
+				htmlFor: string;
+		  }
+	);
+
+/**
+ * Will only render if children is a string or array of strings.
+ */
+export function ErrorText({
+	el = "p",
+	children,
+	htmlFor = undefined,
+}: Props) {
+	if (!children) return null;
+
 	const El = el;
-	return <El className={style.text}>{children}</El>;
+	return (
+		<El className={style.text} htmlFor={htmlFor}>
+			{Array.isArray(children)
+				? children.map((e, i, all) => (
+						// biome-ignore lint/suspicious/noArrayIndexKey: just text, no order logic
+						<Fragment key={i}>
+							{e}
+							{i < all.length - 1 ? <br /> : null}
+						</Fragment>
+					))
+				: children}
+		</El>
+	);
 }
--- src/pages/forgot-password/forgot-password-page.tsx
+++ src/pages/forgot-password/forgot-password-page.tsx
@@ -1,116 +1,16 @@
-import { ErrorText } from "components/error-text/error-text";
-import {
-	Button,
-	Form,
-	Input,
-	Issues,
-} from "components/form";
 import { H1 } from "components/heading/heading";
-import { queryClient } from "lib/api";
 import { usePageTitle } from "lib/page-title";
-import useBackendError from "lib/useBackendError";
-import { V1ForgotPasswordRequestSchema } from "lib/validators.gen";
-import { useEffect, useState } from "react";
-import { Trans, useTranslation } from "react-i18next";
-import { Link } from "react-router";
-import { useZorm } from "react-zorm";
+import { useTranslation } from "react-i18next";
 import style from "./forgot-password-page.module.scss";
 
 const ForgotPasswordPage = () => {
 	const { t } = useTranslation();
 	usePageTitle(t("pages.forgotPassword.title"));
-	const [email, setEmail] = useState("");
-	const [backendErrors, handleBackendErrors] =
-		useBackendError();
-
-	// DEV: showcase what it looks like with backend errors
-	// Normally you would just call `handleBackendErrors` in the `onError` mutation option
-	useEffect(() => {
-		handleBackendErrors({
-			errors: {
-				email: ["Email does not exist"],
-			},
-			message:
-				"Showcase errors on load. This message is a generic error, unrelated to form.",
-		});
-	}, [handleBackendErrors]);
-
-	const { mutate, isPending, isSuccess } =
-		queryClient.useMutation(
-			"post",
-			"/api/v1/auth/forgot-password",
-			{
-				onError(error) {
-					handleBackendErrors(error);
-				},
-			},
-		);
-
-	const zorm = useZorm(
-		"login",
-		V1ForgotPasswordRequestSchema,
-		{
-			onValidSubmit: async (evt) => {
-				evt.preventDefault();
-				setEmail(evt.data.email);
-				mutate({ body: evt.data });
-			},
-			customIssues: backendErrors.formIssues,
-		},
-	);
-
-	if (isSuccess) {
-		return (
-			<>
-				<H1 size="medium" className={style.textCenter}>
-					{t("pages.forgotPassword.title")}
-				</H1>
-				<p className={style.textCenter}>
-					<Trans
-						t={t}
-						i18nKey="pages.forgotPassword.success"
-						values={{ email }}
-						components={{
-							email: <Link to={`mailto:${email}`} />,
-						}}
-					/>
-				</p>
-			</>
-		);
-	}
 
 	return (
-		<>
-			<H1 size="medium" className={style.textCenter}>
-				{t("pages.forgotPassword.title")}
-			</H1>
-			<Form
-				ref={zorm.ref}
-				className={style.form}
-				disabled={isPending}
-			>
-				<label className={style.label} htmlFor="email">
-					<Input
-						label={t("forms.fields.email")}
-						name={zorm.fields.email()}
-						id="email"
-						isInvalid={zorm.errors.email(Boolean)}
-					/>
-					{zorm.errors.email((...issues) => (
-						<Issues issues={issues} />
-					))}
-				</label>
-
-				{backendErrors.genericError && (
-					<ErrorText>
-						{backendErrors.genericError}
-					</ErrorText>
-				)}
-				<Button type="submit">
-					{t("forms.actions.requestNewPassword")}
-				</Button>
-			</Form>
-		</>
+		<H1 size="medium" className={style.textCenter}>
+			{t("pages.forgotPassword.title")}
+		</H1>
 	);
 };
 
