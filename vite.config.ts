22a67dde82c5a31ffbe1f41876f751c07f6725ab
Wouter Lem <32102935+maanlamp@users.noreply.github.com>
2026-05-04 09:54:17 +0200

    chore: Switch to openapi plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import * as path from "node:path";
+import { heyApiPlugin } from "@hey-api/vite-plugin";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import * as path from "node:path";
 import { defineConfig, loadEnv } from "vite";
 import { patchCssModules } from "vite-css-modules";
 import https from "vite-plugin-mkcert";
@@ -13,6 +14,21 @@ export default defineConfig(({ mode }) => {
 
 	return {
 		plugins: [
+			heyApiPlugin({
+				config: {
+					input: "./openapi.json",
+					output: "src/lib/api/heyapi",
+					plugins: [
+						"@hey-api/typescript",
+						"@tanstack/react-query",
+						{
+							name: "@hey-api/sdk",
+							// validator: true, // optional: https://heyapi.dev/openapi-ts/plugins/sdk#validators
+						},
+						"zod",
+					],
+				},
+			}),
 			tanstackRouter({
 				routesDirectory: "./src/routes",
 				autoCodeSplitting: true,
