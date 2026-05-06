b461b403b82cd926a46c16179ca0368e62b309ea
Jorn Luiten <publicJorn@users.noreply.github.com>
2026-05-06 10:44:51 +0200

    Remove css module type generation (#61)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
+import * as path from "node:path";
 import { heyApiPlugin } from "@hey-api/vite-plugin";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import * as path from "node:path";
 import { defineConfig, loadEnv } from "vite";
-import { patchCssModules } from "vite-css-modules";
 import https from "vite-plugin-mkcert";
 import svgr from "vite-plugin-svgr";
 import viteTsConfigPaths from "vite-tsconfig-paths";
@@ -61,13 +60,6 @@ export default defineConfig(({ mode }) => {
 					},
 				},
 			}),
-			// While Vite should automatically handle SASS, it has some problems with modules.
-			// This plugin fixes the issue as the PR isn't ready yet.
-			// https://github.com/vitejs/vite/pull/16018
-			// As a small bonus, it generates types for us :)
-			patchCssModules({
-				generateSourceTypes: true,
-			}),
 		],
 		css: {
 			modules: {
@@ -79,16 +71,38 @@ export default defineConfig(({ mode }) => {
 			rollupOptions: {
 				output: {
 					manualChunks(id) {
-						if (id.includes("/node_modules/react/") || id.includes("/node_modules/react-dom/") || id.includes("/node_modules/scheduler/")) {
+						if (
+							id.includes("/node_modules/react/") ||
+							id.includes("/node_modules/react-dom/") ||
+							id.includes("/node_modules/scheduler/")
+						) {
 							return "vendor-react";
 						}
-						if (id.includes("/node_modules/@tanstack/react-router") || id.includes("/node_modules/@tanstack/router-core") || id.includes("/node_modules/@tanstack/history")) {
+						if (
+							id.includes(
+								"/node_modules/@tanstack/react-router",
+							) ||
+							id.includes(
+								"/node_modules/@tanstack/router-core",
+							) ||
+							id.includes("/node_modules/@tanstack/history")
+						) {
 							return "vendor-router";
 						}
-						if (id.includes("/node_modules/@tanstack/react-query") || id.includes("/node_modules/@tanstack/query-core")) {
+						if (
+							id.includes(
+								"/node_modules/@tanstack/react-query",
+							) ||
+							id.includes(
+								"/node_modules/@tanstack/query-core",
+							)
+						) {
 							return "vendor-query";
 						}
-						if (id.includes("/node_modules/i18next") || id.includes("/node_modules/react-i18next")) {
+						if (
+							id.includes("/node_modules/i18next") ||
+							id.includes("/node_modules/react-i18next")
+						) {
 							return "vendor-i18n";
 						}
 					},
